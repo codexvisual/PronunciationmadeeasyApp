28e0bb8cdecb58fac2d035e74c13a3add37fb494
Rahat <rahat@filofa.com>
2026-08-17 03:46:25 +0600

    Initial commit

---
+++ src/jrbermzkie/pronunciation/CONTENT.java
@@ -0,0 +1,42 @@
+package jrbermzkie.pronunciation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+public class CONTENT extends Activity {
+
+	ImageButton btnContent;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.content);
+		
+		addButtonListener();
+		
+		
+	}
+
+	private void addButtonListener() {
+		// TODO Auto-generated method stub
+		
+		btnContent = (ImageButton) findViewById (R.id.contentBack);
+		btnContent.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Intent x = new Intent("jrbermzkie.pronunciation.PronunciationmadeeasyActivity");
+				startActivity(x);
+				
+			}
+			
+		});
+		
+	}
+
+}
---
+++ src/jrbermzkie/pronunciation/PronunciationmadeeasyActivity.java
@@ -0,0 +1,142 @@
+package jrbermzkie.pronunciation;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+
+public class PronunciationmadeeasyActivity extends Activity {
+    /** Called when the activity is first created. */
+	
+	ImageButton btnStart;
+	ImageButton btnAboutUs;
+	ImageButton btnListWord;
+	ImageButton bTnTutorial;
+	ImageButton btnExit;
+	TextToSpeech ttobj;
+	public MediaPlayer MP;
+	
+	final Context bermzkie = this;
+   
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        addButtonListener();
+        
+        //MP = MediaPlayer.create(PronunciationmadeeasyActivity.this, R.raw.bean);
+	    //MP.start();
+    }
+
+
+	private void addButtonListener() {
+		// TODO Auto-generated method stub
+		
+		//----------- Speak button ------------
+		
+		btnStart = (ImageButton) findViewById(R.id.Start);
+		btnStart.setOnClickListener(new OnClickListener() {
+	
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+			Intent e = new Intent("jrbermzkie.pronunciation.SPEECH");
+			startActivity(e);
+			
+		}
+
+	});
+		
+		//------------------ About us Button -------------------------
+		
+		btnAboutUs = (ImageButton) findViewById (R.id.AboutUs);
+		btnAboutUs.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Intent e = new Intent("jrbermzkie.pronunciation.CONTENT");
+				startActivity(e);
+				
+			}
+		});
+		
+		
+		
+		
+		//------------------ Word List us Button -------------------------
+		
+		btnListWord = (ImageButton) findViewById (R.id.btnWord);
+		btnListWord.setOnClickListener(new OnClickListener() {
+
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						
+						Intent e = new Intent("jrbermzkie.pronunciation.WORD");
+						startActivity(e);
+						
+						
+					}
+				});
+		
+		//------------------ Exit Button -------------------------
+		
+		btnExit = (ImageButton) findViewById (R.id.exit);
+		btnExit.setOnClickListener(new OnClickListener() {
+
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						
+						
+					        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(bermzkie);
+					        alertDialogBuilder.setTitle("Hello boss!");
+					        alertDialogBuilder
+					                .setMessage("Are you sure you want to exit?")
+					                .setCancelable(false)
+					                .setPositiveButton("Yes",
+					                        new DialogInterface.OnClickListener() {
+					                            public void onClick(DialogInterface dialog, int id) {
+					                                moveTaskToBack(true);
+					                                android.os.Process.killProcess(android.os.Process.myPid());
+					                                System.exit(1);
+					                            }
+					                        })
+
+					                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+					                    public void onClick(DialogInterface dialog, int id) {
+
+					                        dialog.cancel();
+					                    }
+					                });
+
+					        AlertDialog alertDialog = alertDialogBuilder.create();
+					        alertDialog.show();
+					    
+					}
+				});
+		
+		
+		//------------------ Tutorial Button -------------------------
+		
+		bTnTutorial = (ImageButton) findViewById (R.id.btnTutorial);
+		bTnTutorial.setOnClickListener(new OnClickListener() {
+
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						
+						Intent e = new Intent("jrbermzkie.pronunciation.LIST");
+						startActivity(e);
+						
+					}
+				});
+		
+		
+}
+}
